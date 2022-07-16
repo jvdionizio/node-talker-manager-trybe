821a020f9d302e6d43c1c26c61f5141566747f20
jvdionizio <dionizio145@gmail.com>
2022-07-16 19:34:53 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs/promises')
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,6 +13,11 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+app.get('/talker', async (_req, res) => {
+  const talker = await fs.readFile('./talker.json', 'utf-8');
+  res.status(HTTP_OK_STATUS).json(JSON.parse(talker));
+});
+
 app.listen(PORT, () => {
-  console.log('Online oi');
+  console.log('Online');
 });
